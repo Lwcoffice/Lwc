7a129654e0600d65752b0126c1bd57e2b38d8530
Lwcoffice <161692224+Lwcoffice@users.noreply.github.com>
2024-02-29 18:25:48 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up!")
+
+# 设置专注时间为25分钟
+countdown(25)
